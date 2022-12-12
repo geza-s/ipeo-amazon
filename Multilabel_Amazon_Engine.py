38f4c6395ba919467ca926cec417122fa86977c8
Geza S <geza.soldati@epfl.ch>
2022-12-12 15:25:34 +0100

    new organisation to run multilabel classification + corrections + working on testing

---
+++ Multilabel_Amazon_Engine.py
@@ -0,0 +1,187 @@
+from tqdm import tqdm
+import torch.nn as nn
+import torchvision.transforms.functional as F
+import numpy as np
+import pandas as pd
+from skimage.io import imread
+import matplotlib.pyplot as plt
+
+
+def checking_folder(data_folder='../IPEO_Planet_project'):
+    labels_dt = pd.read_csv(f'{data_folder}/train_labels.csv', dtype=str)
+    corrupted_files = []
+    i = 0
+    for img_id in tqdm(labels_dt['image_name']):
+        # if i % 1000 == 0:
+        #    print(f"image {i}")
+        img_name = os.path.join(f'{data_folder}/train-jpg', f'{img_id}.jpg')
+        img = imread(img_name)
+        if img is None:
+            print(f'{img_id} is corrupt...')
+            corrupted_files.append(img_id)
+        img = None
+        i = i + 1
+    return corrupted_files
+
+
+def validate(model, dataloader, device, loss_fn=nn.BCEWithLogitsLoss()):
+    sig = nn.Sigmoid()
+    net.eval()
+    acc, tot_loss = [], []
+    print('Validating')
+    with torch.no_grad():
+        for i_batch, sample_batch in tqdm(enumerate(dataloader)):
+            image_batch = sample_batch['image'].to(device)
+            targets = sample_batch['labels'].to(device)
+
+            # Model Predictions
+            out = model(image_batch)
+
+            # Get loss (Sigmoid + Cross Entropy function)
+            loss = loss_fn(out, targets)
+            # Appending it to all the losses
+            tot_loss.append(loss.cpu().detach().numpy())
+
+            # apply sigmoid activation to get all the outputs between 0 and 1
+            predicted = (sig(out) > 0.6).float().cpu().detach().numpy()
+            ground_truth = targets.cpu().detach().numpy()
+            acc.append(np.array((predicted == ground_truth)).astype(np.float64).mean())
+
+    return np.stack(tot_loss).mean(), np.stack(acc).mean()
+
+
+def train_epoch(model, dataloader, device, lr=0.01, optimizer=None, loss_fn=nn.BCEWithLogitsLoss()):
+    """
+    :param model: model used for prediction
+    :param dataloader: dataloader over the amazon space dataset
+    :param device: device on which train, 'cuda' or 'cpu'
+    :param lr: learning rate, here default = 0.01
+    :param optimizer: Here if None given then using Adam
+    :param loss_fn: Loss function default is BCEWithLogitsLoss (sigmoid + cross-entropy)
+    :return:
+    """
+    sig = nn.Sigmoid()
+    optimizer = optimizer or torch.optim.Adam(net.parameters(), lr=lr)
+    model.train()
+    total_loss, acc = [], []
+    print('Training')
+    for i_batch, sample_batch in tqdm(enumerate(dataloader)):
+        # get the inputs.
+        image_batch = sample_batch['image'].to(device)
+        targets = sample_batch['labels'].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # Prediction from model
+        out = model(image_batch)
+
+        # Loss function -> sigmoid included in BCEWithLogistLoss
+        loss = loss_fn(out, targets)
+
+        # backpropagation
+        loss.backward()
+
+        # update optimizer parameters
+        optimizer.step()
+
+        # Metrics
+        # All the losses for this epoch
+        total_loss.append(loss.cpu().detach().numpy())
+        # Prediction of this batch and appending to all accuarcies of this epoch
+        predicted = (sig(out) > 0.6).float().cpu().detach().numpy()
+        ground_truth = targets.cpu().detach().numpy()
+        acc.append(np.array((predicted == ground_truth)).astype(np.float64).mean())
+
+        if i_batch == 0:
+            print(image_batch.size())
+            print(f"Predicted : {predicted} and calculated accuracy: {np.stack(acc).mean()}")
+
+        if i_batch % 100 == 0:  # print every 100 mini-batches the mean loss up to now
+            print(f"loss: {np.stack(total_loss).mean()} and acc: {np.stack(acc).mean()}")
+
+    return np.stack(total_loss).mean(), np.stack(acc).mean()
+
+
+def train(model, train_loader, validation_dataloader, device, optimizer=None, lr=0.01, epochs=2,
+          loss_fn=nn.BCEWithLogitsLoss()):
+    """
+    :param model: model to train
+    :param train_loader: train dataloader
+    :param test_loader: testing dataloader
+    :param optimizer: optimizer per default is Adam
+    :param lr: default 0.01
+    :param epochs: default 2
+    :param loss_fn: default BCEWithLogitsLoss (sigmoid + Cross Entropy)
+    :return: results as a dictionary with 'train_loss', 'train_acc', 'val_loss', 'val_acc'
+    """
+    optimizer = optimizer or torch.optim.Adam(net.parameters(), lr=lr)
+    res = {'train_loss': [], 'train_acc': [], 'val_loss': [], 'val_acc': []}
+    for ep in range(epochs):
+        tl, ta = train_epoch(model, train_loader, optimizer=optimizer, lr=lr, loss_fn=loss_fn, device=device)
+        vl, va = validate(model, validation_dataloader, loss_fn=loss_fn, device=device)
+
+        print(f"Epoch {ep:2}, Train acc={ta:.3f}, Val acc={va:.3f}, Train loss={tl:.3f}, Val loss={vl:.3f}")
+        res['train_loss'].append(tl)
+        res['train_acc'].append(ta)
+        res['val_loss'].append(vl)
+        res['val_acc'].append(va)
+    return res
+
+
+def show_4_image_in_batch(sample_batched, tags):
+    """
+    Shows 4 first images from the batch of the Amazon Dataset
+    :param sample_batched: mini-batch of dataloader of Amazon Dataset. Dictionary with 'image', 'labels'
+    :param tags: All the unique labels
+    :return:
+    """
+    images_batch, labels = sample_batched['image'], sample_batched['labels']
+
+    fig, axs = plt.subplots(1, 4)
+    for i in range(4):
+        img = F.to_pil_image(images_batch[i])
+        axs[i].imshow(img)
+        ids = labels[i, :].numpy()
+        axs[i].set_title(f'#{i}:\n {tags[ids == 1]}')
+    fig.set_figheight(10)
+    fig.set_figwidth(12)
+    plt.tight_layout()
+    plt.show()
+
+
+def batch_prediction(batch, model, device="cuda", criterion = nn.BCEWithLogitsLoss()):
+    """
+    Predict the values from model for batch given. Function for the testing of the model.
+    :param criterion:
+    :param batch: batch composed of 'image' and 'labels'
+    :param model: model of interest
+    :param device: on which device run the thing
+    :return: loss,accuracy
+    """
+    model.eval()
+    sig = nn.Sigmoid()
+
+    # Retrieve image and label from the batch
+    x = batch['image']
+    y = batch['labels']
+
+    # move model and code to GPU
+    model = model.to(device)
+    x = x.to(device)
+    y = y.to(device)
+
+    # Forward pass
+    y_hat = model(x)
+
+    # Loss calculation (only for statistics)
+    loss = criterion(y_hat, y)
+
+    # Calculate accuracy for statistics
+    predicted = (sig(y_hat) > 0.8).float().cpu().detach().numpy()
+    ground_truth = y.cpu().detach().numpy()
+    predictions = np.array((predicted == ground_truth), dtype=np.float64).mean(axis=0)
+    accuracy = (np.array((predicted == ground_truth)).astype(np.float64).mean())
+
+    return loss.cpu().detach().numpy(), accuracy, predictions
+
---
+++ Multilabel_Amazon_Module.py
@@ -0,0 +1,126 @@
+import os
+import torch
+import pandas as pd
+from skimage.io import imread
+import numpy as np
+
+import torchvision.transforms.functional
+from torch.utils.data import Dataset
+from torchvision import transforms
+import torch.nn as nn
+import torchvision.transforms as T
+
+data_folder = '../IPEO_Planet_project'
+if not os.path.exists(data_folder):
+    data_folder = input("Enter the data folder path: ")
+    assert os.path.exists(data_folder), "I did not find the folder at, " + str(data_folder)
+
+
+class AdjustSaturation(object):
+    """Adjust the saturation of a tensor image.
+    Args:
+        saturation factor (float): if 0 -> black and white, if 1 -> same as the input
+    """
+
+    def __init__(self, saturation_factor):
+        assert isinstance(saturation_factor, (int, float))
+        self.saturation_factor = saturation_factor
+
+    def __call__(self, img):
+        new_tensor = transforms.functional.adjust_saturation(img, self.saturation_factor)
+
+        return new_tensor
+
+
+class NormalizeTensor(object):
+    """
+    Normalize image [0-1] (or 0-255) to zero-mean unit standard deviation
+    """
+
+    def __init__(self, mean=(0.504, 0.504, 0.503), std=(0.019, 0.018, 0.018)):
+        assert isinstance(mean, (int, float))
+        assert isinstance(std, (int, float))
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, img):
+        new_tensor = img.Normalize(self.mean, self.std)
+        return new_tensor
+
+
+default_transform = T.Compose([
+    T.Resize((224, 224)),
+    NormalizeTensor])
+
+
+class MultiLayerCNN(nn.Module):
+    def __init__(self):
+        super(MultiLayerCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 10,
+                               kernel_size=5)  # Input is a 3 plane 256x256 tensor (RBB) -> output 10 planes of 254x254
+        self.pool_max = nn.MaxPool2d(2, 2)  # output of dim-2 x dim-2
+        self.conv2 = nn.Conv2d(10, 20, 5)  # input 10 planes 127x127, output 20 planes of 125x125
+        self.pool_avg = nn.AvgPool2d(4, 4)
+        self.fc = nn.Linear(20 * 27 * 27, 17)  # single dense layer for the network
+        self.batchNorm = nn.BatchNorm2d(3)
+        self.loss = nn.BCELoss()
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.batchNorm(x)
+        # print(f"step 1 : {x.shape}")
+        x = self.pool_max(nn.functional.relu(self.conv1(x)))
+        # print(f"step 2 : {x.shape}")
+        x = self.pool_avg(x)
+        # print(f"step 3 : {x.shape}")
+        x = nn.functional.relu(self.conv2(x))
+        # print(f"step 4 : {x.shape}")
+        x = x.view(-1, 20 * 27 * 27)
+        # print(f"step 5 : {x.shape}")
+        x = self.fc(x)
+        return x
+
+
+class AmazonSpaces(Dataset):
+    """Amazon aerial image dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with labels.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.labels = pd.read_csv(csv_file)
+
+        self.tags = self.labels['tags'].str.split(expand=True).stack().unique()
+        for tag in self.tags:
+            a = [(tag in i.split()) for i in self.labels['tags']]
+            self.labels[tag] = np.zeros(len(a), dtype=int) + a
+
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return self.labels.shape[0]
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.labels['image_name'].iloc[idx])
+        img_name = f'{img_name}.jpg'
+        image = imread(img_name)
+
+        if self.transform:
+            image = self.transform(image)
+
+        labels = self.labels.loc[idx, self.tags].to_numpy(dtype=np.float64)
+
+        # other output
+        sample = {'image': image, 'labels': labels}
+
+        # print(type(image), type(labels))
+        return sample
