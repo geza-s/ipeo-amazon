600896ba71b207ac811083d45501b33a8394e769
Geza S <geza.soldati@epfl.ch>
2022-12-31 14:57:01 +0100

    Files for 2nd approach, with errors

---
+++ Multi_Model_Amazon_Engine.py
@@ -0,0 +1,258 @@
+from tqdm import tqdm
+import torch.nn as nn
+from torch import no_grad
+import torchvision.transforms.functional as F
+import numpy as np
+import pandas as pd
+from skimage.io import imread
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, precision_score, recall_score, hamming_loss
+
+
+def validate_dual(ground_model, cloud_model, dataloader, device, ground_loss_fn=nn.BCEWithLogitsLoss(),
+                  cloud_loss_fn=nn.BCELoss()):
+    """
+    Function running validation loop accros the validation dataloader on dual model
+    :param ground_model: ground label model to validate
+    :param cloud_model: cloud detection model to validate
+    :param dataloader: validation dataloader
+    :param device: device : "cuda" or "cpu"
+    :param ground_loss_fn: default = BCEWithLogitsLoss() for multiple ground-label classification
+    :param cloud_loss_fn: default = BCELoss for 3 cloud classes classification
+    :return: 5 numpy arrays for : total loss array, all accuracy array, precision score array,recall score array
+    """
+
+    sig = nn.Sigmoid()
+    smax = nn.Softmax()
+
+    ground_model.eval()
+    cloud_model.eval()
+
+    accs, acc_scores, prec_scores, rec_scores, tot_loss, ham_loss = [], [], [], [], [], []
+    print('Validating')
+    with no_grad():
+        for i_batch, sample_batch in tqdm(enumerate(dataloader)):
+            image_batch = sample_batch['image'].to(device)
+            cloud_target = sample_batch['cloud_target'].to(device)
+            ground_target = sample_batch['ground_target'].to(device)
+
+            # Prediction from model
+            out_clouds = cloud_model(image_batch)
+            out_ground = ground_model(image_batch)
+
+            # Loss function -> sigmoid included in BCEWithLogistLoss
+            loss_clouds = cloud_loss_fn(out_clouds, cloud_target)
+            loss_ground = ground_loss_fn(out_ground, ground_target)
+
+            # Metrics
+            # All the losses for this epoch
+            loss = loss_ground + loss_clouds  # HERE TO CHECK AGAIN WHAT IS TOTAL LOSS !!!
+            tot_loss.append(loss.cpu().detach().item())
+
+            # Prediction of this batch and appending to all accuracies of this epoch
+            predicted_ground = (sig(out_ground) > 0.5).float().cpu().detach().numpy()
+            predicted_cloud = out_clouds.float().cpu().detach().numpy()
+
+            predicted = np.hstack((predicted_ground, predicted_cloud))
+            ground_truth = np.hstack((ground_target.cpu().detach().numpy(), cloud_target.cpu().detach().numpy()))
+
+            # save metrics
+            accs.append(np.mean(np.array(predicted == ground_truth), axis=0).tolist())
+            acc_scores.append(accuracy_score(ground_truth.flatten(), predicted.flatten()))
+            prec_scores.append(precision_score(ground_truth.flatten(), predicted.flatten()))
+            rec_scores.append(recall_score(ground_truth.flatten(), predicted.flatten()))
+            ham_loss.append(hamming_loss(ground_truth.flatten(), predicted.flatten()))
+
+    return tot_loss, accs, acc_scores, prec_scores, rec_scores, ham_loss
+
+
+def train_epoch_dual(cloud_model, ground_model, dataloader, device, lr=0.01, ground_optimizer=None,
+                     cloud_optimizer=None, ground_loss_fn=nn.BCEWithLogitsLoss(), cloud_loss_fn=nn.BCELoss()):
+    """
+    Loop for 1 epoch for a dual model approach
+    :param ground_model: model used for ground prediction
+    :param cloud_model: model used for cloud prediction
+    :param dataloader: dataloader over the amazon space dataset
+    :param device: device on which train, 'cuda' or 'cpu'
+    :param lr: learning rate, here default = 0.01
+    :param ground_optimizer: Here if None given then using Adam
+    :param cloud_optimizer: Here if None given then using Adam
+    :param ground_loss_fn: Loss function default is BCEWithLogitsLoss (sigmoid + cross-entropy)
+    :param cloud_loss_fn: loss function associated with the cloud prediction model
+    :return: [tot_loss, accs, acc_scores, prec_scores, rec_scores, ham_loss]
+    """
+    sig = nn.Sigmoid()
+    clouds_optimizer = cloud_optimizer or torch.optim.Adam(cloud_model.parameters(), lr=lr)
+    ground_optimizer = ground_optimizer or torch.optim.Adam(ground_model.parameters(), lr=lr)
+
+    cloud_model.train()
+    ground_model.train()
+
+    accs, acc_scores, prec_scores, rec_scores, tot_loss, ham_loss = [], [], [], [], [], []
+
+    print('Training')
+    for i_batch, sample_batch in tqdm(enumerate(dataloader)):
+        # get the inputs.
+        image_batch = sample_batch['image'].to(device)
+        cloud_target = sample_batch['cloud_target'].to(device)
+        ground_target = sample_batch['ground_target'].to(device)
+
+        # zero the parameter gradients
+        clouds_optimizer.zero_grad()
+        ground_optimizer.zero_grad()
+
+        # Prediction from model
+        out_clouds = cloud_model(image_batch)
+        out_ground = ground_model(image_batch)
+
+        # Loss function -> sigmoid included in BCEWithLogistLoss
+        loss_clouds = cloud_loss_fn(out_clouds, cloud_target)
+        loss_ground = ground_loss_fn(out_ground, ground_target)
+
+        # backpropagation
+        loss_clouds.backward()
+        loss_ground.backward()
+
+        # update optimizer parameters
+        clouds_optimizer.step()
+        ground_optimizer.step()
+
+        # Metrics
+        # All the losses for this epoch
+        loss = loss_ground + loss_clouds  # HERE TO CHECK AGAIN WHAT IS TOTAL LOSS !!!
+        tot_loss.append(loss.cpu().detach().item())
+
+        # Prediction of this batch and appending to all accuarcies of this epoch
+        predicted_ground = (sig(out_ground) > 0.5).float().cpu().detach().numpy()
+        predicted_cloud = out_clouds.float().cpu().detach().numpy()
+
+        predicted = np.hstack((predicted_ground, predicted_cloud))
+        ground_truth = np.hstack((ground_target.cpu().detach().numpy(), cloud_target.cpu().detach().numpy()))
+
+        # save all the metrics
+        accs.append(np.mean(np.array(predicted == ground_truth), axis=0).tolist())
+        acc_scores.append(accuracy_score(ground_truth.flatten(), predicted.flatten()))
+        prec_scores.append(precision_score(ground_truth.flatten(), predicted.flatten()))
+        rec_scores.append(recall_score(ground_truth.flatten(), predicted.flatten()))
+        ham_loss.append(hamming_loss(ground_truth.flatten(), predicted.flatten()))
+
+        if i_batch == 3:
+            print(image_batch.size())
+            print(np.shape(predicted), np.shape(ground_truth))
+            print(
+                f"Predicted : {predicted}, calculated accuracy score: {np.mean(acc_scores)}, prediction score : {np.mean(prec_scores)}, recall score: {np.mean(rec_scores)}")
+            break
+        if i_batch % 20 == 0:  # print every ... mini-batches the mean loss up to now
+            print(
+                f"Loss : {np.mean(tot_loss)}, calculated accuracy score: {np.mean(acc_scores)}, prediction score : {np.mean(prec_scores)}, recall score: {np.mean(rec_scores)}")
+
+    return tot_loss, accs, acc_scores, prec_scores, rec_scores, ham_loss
+
+
+def train_dual(ground_model, cloud_model, train_loader, validation_dataloader, device, ground_optimizer=None,
+               cloud_optimizer=None, lr=0.01, epochs=2, ground_loss_fn=None, cloud_loss_fn=None):
+    """
+    :param ground_model: model for ground label detection
+    :param cloud_model: model for cloud detection
+    :param train_loader: train dataloader
+    :param validation_dataloader : dataloader associated to validation
+    :param device: device for training
+    :param ground_optimizer: optimizer per default is Adam
+    :param cloud_optimizer: optimizer per default is Adam
+    :param lr: default 0.01
+    :param epochs: default 2
+    :param ground_loss_fn: default BCEWithLogitsLoss (sigmoid + Cross Entropy)
+    :param cloud_loss_fn: default BCELoss (after softmax)
+    :return: results as a dictionary with 'train_loss', 'train_acc', 'train_acc_scores', 'train_prec_scores',
+     'train_rec_scores', 'train_ham_loss', 'val_loss', 'val_acc', 'val_acc_scores', 'val_prec_scores',
+     'val_rec_scores', 'val_ham_loss'
+    """
+
+    ground_optimizer = ground_optimizer or torch.optim.Adam(ground_model.parameters(), lr=lr)
+    cloud_optimizer = cloud_optimizer or torch.optim.Adam(cloud_model.parameters(), lr=lr)
+
+    res = {'train_loss': [], 'train_acc': [], 'train_acc_scores': [], 'train_prec_scores': [], 'train_rec_scores': [],
+           'train_ham_loss': [],
+           'val_loss': [], 'val_acc': [], 'val_acc_scores': [], 'val_prec_scores': [], 'val_rec_scores': [],
+           'val_ham_loss': []}
+    for ep in range(epochs):
+        tl, ta, a_s, p_s, r_s, h_l = train_epoch_dual(cloud_model, ground_model, train_loader, device=device,
+                                                      ground_optimizer=ground_optimizer,
+                                                      cloud_optimizer=cloud_optimizer, lr=lr,
+                                                      ground_loss_fn=ground_loss_fn,
+                                                      cloud_loss_fn=cloud_loss_fn)
+        vl, va, va_s, vp_s, vr_s, vh_l = validate_dual(ground_model, cloud_model, validation_dataloader,
+                                                       ground_loss_fn=ground_loss_fn,
+                                                       cloud_loss_fn=cloud_loss_fn, device=device)
+
+        # print(f"Epoch {ep:2}, Train acc={ta:.3f}, Val acc={va:.3f}, Train loss={tl:.3f}, Val loss={vl:.3f}")
+        res['train_loss'].append(tl)
+        res['train_acc'].append(ta)
+        res['train_acc_scores'].append(a_s)
+        res['train_prec_scores'].append(p_s)
+        res['train_rec_scores'].append(r_s)
+        res['train_ham_loss'].append(h_l)
+
+        res['val_loss'].append(vl)
+        res['val_acc'].append(va)
+        res['val_acc_scores'].append(va_s)
+        res['val_prec_scores'].append(vp_s)
+        res['val_rec_scores'].append(vp_s)
+        res['val_ham_loss'].append(vh_l)
+    return res
+
+
+def batch_prediction_dual(batch, ground_model, cloud_model, device="cuda", ground_criterion=nn.BCEWithLogitsLoss(),
+                          cloud_criterion=nn.BCELoss()):
+    """
+    Predict the values from model for batch given. Function for the testing of the model.
+    :param batch: batch composed of 'image' and 'labels'
+    :param ground_model: model trained for ground detection
+    :param cloud_model: model trained for cloud detection
+    :param device: on which device run the thing
+    :param ground_criterion: for ground loss value
+    :param cloud_criterion: for cloud loss value
+    :return: loss,accuracy
+    """
+    ground_model.eval()
+    cloud_model.eval()
+
+    sig = nn.Sigmoid()
+    smax = nn.Softmax()
+
+    # Retrieve image and label from the batch and
+    x = batch['image'].to(device)
+    y_ground = batch['ground_target'].to(device)
+    y_cloud = batch['cloud_target'].to(device)
+
+    # move model and code to GPU
+    ground_model = ground_model.to(device)
+    cloud_model = cloud_model.to(device)
+
+    # Forward pass
+    y_hat_ground = ground_model(x)
+    y_hat_cloud = cloud_model(x)
+
+    # Loss calculation (only for statistics)
+    ground_loss = ground_criterion(y_hat_ground, y_ground)
+    cloud_loss = cloud_criterion(y_hat_cloud, y_cloud)
+
+    loss = ground_loss + cloud_loss  ## HERE ALSO TO MODIFY ACCORDING TO LOSS
+
+    # THe predictions
+    ground_predicted = (sig(y_hat_ground) > 0.5).float().cpu().detach().numpy()
+    cloud_predicted = smax(y_cloud).cpu().detach().numpy()
+    predicted = np.hstack((ground_predicted, cloud_predicted))
+
+    # The targeted values
+    ground_target = y_ground.cpu().detach().numpy()
+    cloud_target = y_cloud.cpu().detach().numpy()
+    ground_truth = np.hstack((ground_target, cloud_target))
+
+    # The according metrics
+    predictions = np.array((predicted == ground_truth), dtype=np.float64).mean(axis=0)
+    accuracy = (np.array((predicted == ground_truth)).astype(np.float64).mean())
+
+    return loss.cpu().detach().numpy(), accuracy, predictions
+
+#%%
---
+++ Multi_Model_Amazon_Module.py
@@ -0,0 +1,151 @@
+import os
+import torch
+import pandas as pd
+from skimage.io import imread
+import numpy as np
+
+import torchvision.transforms.functional
+from torch.utils.data import Dataset
+from torchvision import transforms
+import torch.nn as nn
+import torchvision.transforms as T
+
+data_folder = '../IPEO_Planet_project'
+if not os.path.exists(data_folder):
+    data_folder = input("Enter the data folder path: ")
+    assert os.path.exists(data_folder), "I did not find the folder at, " + str(data_folder)
+
+
+class AdjustSaturation(object):
+    """Adjust the saturation of a tensor image.
+    Args:
+        saturation factor (float): if 0 -> black and white, if 1 -> same as the input
+    """
+
+    def __init__(self, saturation_factor):
+        assert isinstance(saturation_factor, (int, float))
+        self.saturation_factor = saturation_factor
+
+    def __call__(self, img):
+        new_tensor = transforms.functional.adjust_saturation(img, self.saturation_factor)
+
+        return new_tensor
+
+
+class GroundCNN(nn.Module):
+    def __init__(self):
+        super(GroundCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 10,
+                               kernel_size=5)  # Input is a 3 plane 256x256 tensor (RBB) -> output 10 planes of 254x254
+        self.pool_max = nn.MaxPool2d(2, 2)  # output of dim-2 x dim-2
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)  # input 10 planes 127x127, output 20 planes of 125x125
+        self.pool_avg = nn.AvgPool2d(4, 4)
+        self.fc = nn.Linear(20 * 27 * 27, 14)  # single dense layer for the network
+        self.batchNorm = nn.BatchNorm2d(3)
+        self.loss = nn.BCELoss()
+
+    def forward(self, x):
+        x = self.batchNorm(x)
+        # print(f"step 1 : {x.shape}")
+        x = self.pool_max(nn.functional.relu(self.conv1(x)))
+        # print(f"step 2 : {x.shape}")
+        x = self.pool_avg(x)
+        # print(f"step 3 : {x.shape}")
+        x = nn.functional.relu(self.conv2(x))
+        # print(f"step 4 : {x.shape}")
+        x = x.view(-1, 20 * 27 * 27)
+        # print(f"step 5 : {x.shape}")
+        x = self.fc(x)
+        return x
+
+
+class CloudCNN(nn.Module):
+    def __init__(self):
+        super(CloudCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 10,
+                               kernel_size=10)  # Input is a 3 plane 256x256 tensor (RBB) ->
+        # output 10 planes of 218x218
+        self.pool_max = nn.MaxPool2d(4, 4)  # output of dim-2 x dim-2
+        self.conv2 = nn.Conv2d(10, 20, 5)  # input 10 planes 127x127, output 20 planes of 125x125
+        self.pool_avg = nn.AvgPool2d(2, 2)
+        self.fc = nn.Linear(13520, 3)  # single dense layer for the network
+        self.batchNorm = nn.BatchNorm2d(3)
+        self.sig = nn.Sigmoid()
+        self.dropped = nn.Dropout(p=0.1, inplace=False)
+        self.smax = nn.Softmax()
+
+    def forward(self, x):
+        # x = self.conv1(x)
+        # print(f"step 0 : {x.shape}")
+        x = self.batchNorm(x)
+        print(f"step 1 : {x.shape}")
+        x = self.dropped(self.pool_max(nn.functional.relu(self.conv1(x))))
+        print(f"step 2 : {x.shape}")
+        x = self.pool_avg(x)
+        print(f"step 3 : {x.shape}")
+        x = self.dropped(nn.functional.relu(self.conv2(x)))
+        print(f"step 4 : {x.shape}")
+        x = x.view(-1, 20 *26*26)
+        print(f"step 5 : {x.shape}")
+        x = self.fc(x)
+        print(f"step 6 : {x.shape}")
+        return self.smax(x)
+
+
+class AmazonSpaceDual(Dataset):
+    """Amazon aerial image dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with labels.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on each batch.
+        """
+        # A Panda's table for tags associated with images.
+        # 2 columns: "image_name" and "tags"
+        self.labels = pd.read_csv(csv_file)
+
+        # Dictionaries listing all the tags
+        self.tags_cloud = {'clear': 0, 'partly_cloudy': 1, 'cloudy': 2}
+        self.tags_ground = {'haze': 0, 'primary': 1, 'agriculture': 2, 'water': 3, 'habitation': 4, 'road': 5, 'cultivation': 6, 'slash_burn': 7, 'conventional_mine': 8, 'bare_ground': 9, 'artisinal_mine': 10, 'blooming': 11, 'selective_logging': 12, 'blow_down': 13}
+
+        # Creation of binary tag presence for optimisation of model
+        for tag in self.tags_ground.keys():
+            a = [(tag in i.split()) for i in self.labels['tags']]
+            self.labels[tag] = np.zeros(len(a), dtype=int) + a
+        for tag in self.tags_cloud.keys():
+            a = [(tag in i.split()) for i in self.labels['tags']]
+            self.labels[tag] = np.zeros(len(a), dtype=int) + a
+
+        # Other information
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return self.labels.shape[0]
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        # Reading the image
+        img_name = os.path.join(self.root_dir,
+                                self.labels['image_name'].iloc[idx])
+        img_name = f'{img_name}.jpg'
+        image = imread(img_name)
+
+        # Applying given transforms
+        if self.transform:
+            image = self.transform(image)
+
+
+        # Getting the target values
+        cloud_target = self.labels.loc[idx, self.tags_cloud.keys()].to_numpy(dtype=np.float32)
+        ground_target = self.labels.loc[idx, self.tags_ground.keys()].to_numpy(dtype=np.float32)
+
+        # other output
+        sample = {'image': image, 'cloud_target': cloud_target, 'ground_target': ground_target}
+
+        return sample
